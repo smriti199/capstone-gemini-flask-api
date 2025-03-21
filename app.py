6ac996b0b25f48f9790651c09abc3795678d36e7
smriti199 <smriti007sgp@gmail.com>
2025-03-21 13:59:49 +0800

    redid cors

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify, CORS
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
